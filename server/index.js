4a8a1a1433984ac1a5c02fe48e9b37302a5cddaa
vaibhavb1998 <vaibhavbajpai12@gmail.com>
2021-08-01 18:12:55 +0530

    test error log

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
